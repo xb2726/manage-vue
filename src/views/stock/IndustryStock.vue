d4801015bcc1bf55cf35365795ee226c2ccca3c9
xiabin <272676785@qq.com>
2022-09-07 08:59:09 +0800

    个股页面

--- src/views/stock/IndustryStock.vue
+++ src/views/stock/IndustryStock.vue
@@ -13,75 +13,84 @@
       <!-- 条件查询设置区 -->
       <div class="handle-box">
         <el-input
-          v-model="param.title"
-          placeholder="标题"
-          class="handle-input mr10"
+            v-model="param.title"
+            placeholder="标题"
+            class="handle-input mr10"
         ></el-input>
         <el-button type="primary" icon="el-icon-search" @click="handleSearch"
-          >搜索</el-button
+        >搜索
+        </el-button
+        >
+        <el-button type="primary" icon="el-icon-refresh" @click="zcSearch"
+        >政策
+        </el-button
         >
 
         <el-button type="warning" icon="el-icon-refresh" @click="reset"
-        >重置</el-button
+        >重置
+        </el-button
         >
 
       </div>
       <!-- 表单区 -->
       <el-table
-        :data="tableData"
-        class="table"
-        ref="multipleTable"
-        header-cell-class-name="table-header"
-        table-layout="auto"
-        :row-class-name="tableRowClassName"
+          :data="tableData"
+          class="table"
+          ref="multipleTable"
+          header-cell-class-name="table-header"
+          table-layout="auto"
+          :row-class-name="tableRowClassName"
       >
         <el-table-column
-          type="index"
-          :index="indexMethod"
-          label="ID"
-          width="55"
-          align="center"
+            type="index"
+            :index="indexMethod"
+            label="ID"
+            width="55"
+            align="center"
         ></el-table-column>
         <el-table-column label="标题" show-overflow-tooltip="true">
           <template #default="scope">
             <div title="点击查看">
-              <el-link type="primary" :underline="false" :href="scope.row.pdfUrl" @click="handleRead(scope.row.id)" target="_blank" style="overflow: hidden;text-overflow: ellipsis;white-space: nowrap;display: block">{{
-                scope.row.title
-              }}</el-link>
+              <el-link type="primary" :underline="false" :href="scope.row.pdfUrl" @click="handleRead(scope.row.id)"
+                       target="_blank"
+                       style="overflow: hidden;text-overflow: ellipsis;white-space: nowrap;display: block">{{
+                  scope.row.title
+                }}
+              </el-link>
             </div>
           </template>
         </el-table-column>
         <el-table-column
-          prop="industryName"
-          label="行业"
-          width="180"
+            prop="industryName"
+            label="行业"
+            width="180"
         ></el-table-column>
         <el-table-column
-          prop="orgSName"
-          label="券商"
-          width="180"
+            prop="orgSName"
+            label="券商"
+            width="180"
         ></el-table-column>
         <el-table-column
-          prop="publishDate"
-          label="时间"
-          width="180"
+            prop="publishDate"
+            label="时间"
+            width="180"
         ></el-table-column>
         <el-table-column
-          prop="emRatingName"
-          label="评级"
-          width="100"
+            prop="emRatingName"
+            label="评级"
+            width="100"
         ></el-table-column>
       </el-table>
 
       <!-- 页码区 -->
       <div class="pagination">
         <el-pagination
-          background
-          layout="total, prev, pager, next"
-          :current-page="param.pageNum"
-          :page-size="param.pageSize"
-          :total="pageTotal"
-          @current-change="handlePageChange"
+            background
+            layout="total, prev, pager, next"
+            :current-page="param.pageNum"
+            :page-size="param.pageSize"
+            :total="pageTotal"
+            @current-change="handlePageChange"
         ></el-pagination>
       </div>
     </div>
@@ -89,11 +98,11 @@
 </template>
 
 <script>
-import { ref, reactive } from "vue";
-import { pageQuery,readRecord } from "../../api/industrystock";
+import {ref, reactive} from "vue";
+import {pageQuery, readRecord} from "../../api/industrystock";
 
 export default {
-  setup () {
+  setup() {
 
     const param = reactive({
       title: "",
@@ -119,13 +128,18 @@ export default {
       param.pageNum = 1;
       getData();
     };
+    const zcSearch = () => {
+      param.pageNum = 1;
+      param.title='政策';
+      getData();
+    }
     //重置
-    const reset=()=>{
-      param.title='';
+    const reset = () => {
+      param.title = '';
     };
 
     const tableRowClassName = ({row, rowIndex}) => {
-      if (row.reserved1!==undefined) {
+      if (row.reserved1 !== undefined) {
         return 'success-row'
       }
       return ''
@@ -156,6 +170,7 @@ export default {
       pageTotal,
       getData,
       handleSearch,
+      zcSearch,
       reset,
       handlePageChange,
       indexMethod,
@@ -177,15 +192,18 @@ export default {
 .handle-box {
   margin-bottom: 20px;
 }
+
 .handle-input {
   width: 300px;
   display: inline-block;
 }
+
 .table {
   width: 100%;
   font-size: 14px;
   cursor: pointer;
 }
+
 .mr10 {
   margin-right: 10px;
 }
